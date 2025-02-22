b69d4ca7e2a69fa44a4552d1797851c062f7e0a5
Himanshu <himanshu.a.04.1999@gmail.com>
2025-02-22 10:25:03 -0500

    Fix mongo warnings
    They were coming from the options passed in connection to mongo db

--- actions/general.js
+++ actions/general.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 async function connectToDb() {
   await mongoose.connect(process.env.MONGODB_URI, {
     dbName: "tasks_next", // ✅ Force MongoDB to use "tasks_next"
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   });
 }
 
