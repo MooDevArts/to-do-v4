d2ce303253a3bbe99ad503aec533cb3ff4a2a5b0
Himanshu <himanshu.a.04.1999@gmail.com>
2025-02-23 17:53:50 -0500

    comment out w/o mongoose line

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default async function Home() {
   await addTask("Mongoose", "V4 bro pls work");
-  await addTaskv2("W/o mongoose", "V4 bro pls work");
+  // await addTaskv2("W/o mongoose", "V4 bro pls work");
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
